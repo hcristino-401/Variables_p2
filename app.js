e5fce1ca5b8a56c718c78268bf8cde2ed3d492b3
Denise cristino <hcristino@student.alamo.edu>
2026-02-06 15:11:42 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+//my favorite number 
+const favoriteNumber = 24;
+console.log("favorite Numbe is:", favoriteNumber);
+
+if (true) {
+    //calculate score);
+    let score = 99;
+    console.log("Updated score:", score);
+
+    //mathematical operation
+    score = score * 3;
+    console.log("Block variable-Update score:", score);
+}
+//Storing a name
+const name = "Denise Cristino"; 
+console.log("string variable-String variable - Name:", name);
+
+//object variable using const
+const person = {
+    name: name,
+    age: 24,
+    score: 200
+};
+console.log("object variable-person", person);
